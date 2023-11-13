52838975af616b81da7de1b443de8bf6fc4602d1
ks0927 <brian_als@naver.com>
2023-11-13 16:40:51 +0900

    test: StarEvent 클래스 테스트

---
+++ src/test/java/christmas/StarEventTest.java
@@ -0,0 +1,31 @@
+package christmas;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("특별 할인 테스트")
+class StarEventTest {
+    @Test
+    @DisplayName("달력이 별이 있는날 할인 금액 테스트")
+    void discountAmountOnEventDay() {
+        VisitDate visitDate = VisitDate.from(3);
+        Event starEvent = new StarEvent(visitDate);
+
+        int discountAmount = starEvent.discountAmount();
+
+        assertThat(discountAmount).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("달력이 별이 없는날 할인 금액 테스트")
+    void discountAmountOnNonEventDay() {
+        VisitDate visitDate = VisitDate.from(1);
+        Event starEvent = new StarEvent(visitDate);
+
+        int discountAmount = starEvent.discountAmount();
+
+        assertThat(discountAmount).isEqualTo(0);
+    }
+}
