37e17f8639b6b2be9de1bb0b765cba532ac50b64
ks0927 <brian_als@naver.com>
2023-11-13 16:40:36 +0900

    feat: 특별 할인 클래스 추가

---
+++ src/main/java/christmas/StarEvent.java
@@ -0,0 +1,23 @@
+package christmas;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StarEvent implements Event {
+    private static final List<Integer> EVENT_DAYS = Arrays.asList(3, 10, 17, 24, 25, 31);
+    private static final int SPECIAL_DISCOUNT = 1000;
+
+    private final VisitDate visitDate;
+
+    public StarEvent(VisitDate visitDate) {
+        this.visitDate = visitDate;
+    }
+
+    @Override
+    public int discountAmount() {
+        if (EVENT_DAYS.contains(visitDate.getDate())) {
+            return SPECIAL_DISCOUNT;
+        }
+        return 0;
+    }
+}
