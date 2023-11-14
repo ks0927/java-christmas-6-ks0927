40a829c6af91270512561e4b2d2c0d513081eab0
ks0927 <brian_als@naver.com>
2023-11-15 00:46:09 +0900

    refactor: Mapper를 InputView에 종속적이게 변경

---
+++ src/main/java/christmas/view/InputMapper.java
@@ -0,0 +1,65 @@
+package christmas.view;
+
+import christmas.Menu;
+import christmas.Orders;
+import christmas.PromotionException;
+import christmas.VisitDate;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class InputMapper {
+    public VisitDate mapToVisitDate(String input) {
+        int dayOfMonth;
+        try {
+            dayOfMonth = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(PromotionException.dateException());
+        }
+        return VisitDate.from(dayOfMonth);
+    }
+
+    public Orders mapToOrders(String input) {
+        String[] orders = input.split(",", -1);
+
+        Map<Menu, Integer> orderMap = Arrays.stream(orders)
+                .map(order -> validateOrderFormat(order))
+                .collect(Collectors.toMap(
+                        details -> validateMenu(details[0]),
+                        details -> validateQuantity(details[1]),
+                        (existing, replacement) -> {
+                            throw new IllegalArgumentException(PromotionException.orderException());
+                        }));
+
+        return new Orders(orderMap);
+    }
+
+    private String[] validateOrderFormat(String input) {
+        String[] details = input.split("-");
+        if (details.length != 2) {
+            throw new IllegalArgumentException(PromotionException.orderException());
+        }
+        return details;
+    }
+
+    private Menu validateMenu(String input) {
+        return Arrays.stream(Menu.values())
+                .filter(menu -> menu.getName().equals(input))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(PromotionException.orderException()));
+    }
+
+    private int validateQuantity(String input) {
+        int quantity;
+        try {
+            quantity = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(PromotionException.orderException());
+        }
+
+        if (quantity < 1) {
+            throw new IllegalArgumentException(PromotionException.orderException());
+        }
+        return quantity;
+    }
+}
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -6,6 +6,12 @@ public class InputView {
     private static final String READ_VISIT_DATE_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
     private static final String READ_ORDERS_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
 
+    private final InputMapper inputMapper;
+
+    public InputView(InputMapper inputMapper) {
+        this.inputMapper = inputMapper;
+    }
+
     public String readVisitDate() {
         System.out.println(READ_VISIT_DATE_MESSAGE);
         return Console.readLine();
